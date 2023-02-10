3472d6bdc1382d36de9a8960718c48ec09b0bad3
shirara1016 <shirara1016@gmail.com>
2023-02-10 11:21:00 +0900

    amend setup and add lisence

--- setup.py
+++ setup.py
@@ -1,16 +1,27 @@
 from setuptools import find_packages, setup
 
+with open('README.md', 'r', encoding='utf8') as f:
+    readme = f.read()
+
+with open('LICENSE', 'r', encoding='utf8') as f:
+    license = f.read()
+
 setup(
-    name="sicore",
-    version="0.1.0",
-    description="Core package for Selective Inference",
-    packages=find_packages(exclude=["tests"]),
+    name='sicore',
+    version='1.0.0',
+    description='Core package for Selective Inference',
+    long_description=readme,
+    author='Takeuchi Lab',
+    author_email='shiraishi.tomohiro.nagoyaml@gmail.com',
+    url='https://github.com/shirara1016/sicore',
+    packages=find_packages(exclude=('tests', 'docs')),
     install_requires=[
-        "numpy>=1.19.1",
-        "mpmath>=1.1.0",
-        "matplotlib>=3.3.1",
-        "scipy>=1.5.2",
-        "statsmodels>=0.11.1",
+        'numpy>=1.19.1',
+        'mpmath>=1.1.0',
+        'matplotlib>=3.3.1',
+        'scipy>=1.5.2',
+        'statsmodels>=0.11.1',
     ],
-    python_requires=">=3.6",
+    python_requires='>=3.6',
+    license='MIT License'
 )
