1a2cdc72103b314b0df431ff66926b95fc5c7123
shirara1016 <shirara1016@gmail.com>
2024-08-18 19:52:25 +0900

    update docstring for initializer

--- sicore/main/inference.py
+++ sicore/main/inference.py
@@ -36,7 +36,24 @@ class SelectiveInferenceNorm(SelectiveInference):
         use_tf: bool = False,
         use_torch: bool = False,
     ):
-        """Initialize a SelectiveInferenceNorm object."""
+        """Initialize a SelectiveInferenceNorm object.
+
+        Args:
+            data (np.ndarray): Observed data in 1D array.
+            var (float | np.ndarray | sparse.csr_matrix): Known covariance matrix.
+                If float, covariance matrix equals to the scalar times identity matrix.
+                If 1D array, covariance matrix equals to the diagonal matrix with the given array.
+                If 2D array, covariance matrix equals to the given array.
+            eta (np.ndarray): The direction of the test statistic in 1D array.
+            use_sparse (bool, optional): Whether to use sparse matrix.
+                If True, the `var` must be given as a sparse matrix. Defaults to False.
+            use_tf (bool, optional): Whether to use TensorFlow.
+                If True, the `data`, `eta`, and `var` must be given as TensorFlow tensors.
+                Defaults to False.
+            use_torch (bool, optional): Whether to use PyTorch.
+                If True, the `data`, `eta`, and `var` must be given as PyTorch tensors.
+                Defaults to False.
+        """
         if np.sum([use_sparse, use_tf, use_torch]) > 1:
             raise ValueError(
                 "Only one of use_sparse, use_tf, and use_torch can be True."
@@ -142,7 +159,21 @@ class SelectiveInferenceChi(SelectiveInference):
         use_tf: bool = False,
         use_torch: bool = False,
     ):
-        """Initialize a SelectiveInferenceChi object."""
+        """Initialize a SelectiveInferenceChi object.
+
+        Args:
+            data (np.ndarray): Observed data in 1D array.
+            var (float): Known covariance matrix, which equals to the scalar times identity matrix.
+            P (np.ndarray): The space of the test statistic in 2D array.
+            use_sparse (bool, optional): Whether to use sparse matrix.
+                If True, the `P` must be given as a sparse matrix. Defaults to False.
+            use_tf (bool, optional): Whether to use TensorFlow.
+                If True, the `data` and `P` must be given as TensorFlow tensors.
+                Defaults to False.
+            use_torch (bool, optional): Whether to use PyTorch.
+                If True, the `data` and `P` must be given as PyTorch tensors.
+                Defaults to False.
+        """
         if np.sum([use_sparse, use_tf, use_torch]) > 1:
             raise ValueError(
                 "Only one of use_sparse, use_tf, and use_torch can be True."
