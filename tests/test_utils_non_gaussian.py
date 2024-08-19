a74365a03641f911f3aa0e0fadad33bcaa2b89aa
shirara1016 <shirara1016@gmail.com>
2024-08-19 21:34:09 +0900

    slim down test for non gaussian random variable

--- tests/test_utils_non_gaussian.py
+++ tests/test_utils_non_gaussian.py
@@ -1,39 +1,14 @@
 import pytest
-import numpy as np
 from numpy.testing import assert_allclose
-from itertools import product
 from sicore.utils.non_gaussian import generate_non_gaussian_rv, _wasserstein_distance
 
 
 @pytest.mark.parametrize(
-    "rv_name, distance",
-    [
-        (rv_name, distance)
-        for rv_name, distance in product(
-            ["t", "skewnorm", "gennormsteep", "gennormflat", "exponnorm"],
-            [
-                0.01,
-                0.02,
-                0.03,
-                0.04,
-                0.05,
-                0.06,
-                0.07,
-                0.08,
-                0.09,
-                0.10,
-                0.11,
-                0.12,
-                0.13,
-                0.14,
-                0.15,
-            ],
-        )
-    ],
+    "rv_name",
+    ["t", "skewnorm", "gennormsteep", "gennormflat", "exponnorm"],
 )
-def test_generate_non_gaussian_rv(rv_name, distance):
-    rv = generate_non_gaussian_rv(rv_name, distance)
-    value = _wasserstein_distance(rv)
-    assert_allclose(value, distance, rtol=1e-6, atol=1e-6)
+def test_generate_non_gaussian_rv(rv_name):
+    rv = generate_non_gaussian_rv(rv_name, 0.03)
+    assert_allclose(_wasserstein_distance(rv), 0.03)
     assert_allclose(rv.mean(), 0.0, rtol=1e-6, atol=1e-6)
     assert_allclose(rv.std(), 1.0, rtol=1e-6, atol=1e-6)
