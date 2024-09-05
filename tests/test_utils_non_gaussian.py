6b410b775351b7567f72c87c734da767428cf5e0
shirara1016 <shirara1016@gmail.com>
2024-09-05 13:29:30 +0900

    update test for non gaussian

--- tests/test_utils_non_gaussian.py
+++ tests/test_utils_non_gaussian.py
@@ -2,10 +2,18 @@
 
 from typing import Literal
 
+import numpy as np
 import pytest
 from numpy.testing import assert_allclose
+from scipy.integrate import quad  # type: ignore[import]
+from scipy.stats import norm, rv_continuous  # type: ignore[import]
 
-from sicore.utils.non_gaussian import _wasserstein_distance, generate_non_gaussian_rv
+from sicore.utils.non_gaussian import generate_non_gaussian_rv
+
+
+def wasserstein_distance(rv: rv_continuous) -> float:
+    """Return the wasserstein distance of the random variable."""
+    return quad(lambda z: np.abs(rv.cdf(z) - norm.cdf(z)), -np.inf, np.inf)[0]
 
 
 @pytest.mark.parametrize(
@@ -17,6 +25,6 @@ def test_generate_non_gaussian_rv(
 ) -> None:
     """Test the generate non-gaussian random variable function."""
     rv = generate_non_gaussian_rv(rv_name, 0.03)
-    assert_allclose(_wasserstein_distance(rv), 0.03)
+    assert_allclose(wasserstein_distance(rv), 0.03)
     assert_allclose(rv.mean(), 0.0, rtol=1e-6, atol=1e-6)
     assert_allclose(rv.std(), 1.0, rtol=1e-6, atol=1e-6)
