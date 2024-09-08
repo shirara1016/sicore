a3759438ed3b56e76082215b2c4684c587c2231b
shirara1016 <shirara1016@gmail.com>
2024-09-08 20:40:34 +0900

    improve truncated cdf function and add theire tests

--- sicore/core/cdf.py
+++ sicore/core/cdf.py
@@ -4,6 +4,8 @@ from collections.abc import Callable
 
 import numpy as np
 from mpmath import mp  # type: ignore[import]
+from scipy.special import logsumexp  # type: ignore[import]
+from scipy.stats import rv_continuous  # type: ignore[import]
 
 from .real_subset import NotBelongToSubsetError, RealSubset
 
@@ -184,3 +186,91 @@ def truncated_chi_cdf(
             absolute=absolute,
         ),
     )
+
+
+def truncated_cdf(
+    rv: rv_continuous,
+    z: float,
+    intervals: np.ndarray | list[list[float]] | RealSubset,
+    *,
+    absolute: bool = False,
+) -> float:
+    """Compute the cdf value of the truncated distribution.
+
+    Args:
+        rv (rv_continuous): The rv_continuous instance to be truncated.
+        z (float): The value at which to compute the cdf of the truncated distribution.
+        intervals (np.ndarray | list[list[float]] | RealSubset): The truncated intervals
+            [[l1, u1], [l2, u2], ...].
+        absolute (bool, optional): Whether to compute the cdf for the distribution of the
+            absolute value of the random variable. Defaults to False.
+
+    Returns:
+        float: The cdf value at z.
+
+    Raises:
+        ValueError: If the value z is not belong to the truncated intervals.
+    """
+    if not isinstance(intervals, RealSubset):
+        intervals = RealSubset(intervals)
+
+    if z not in intervals:
+        raise NotBelongToSubsetError(z, intervals)
+
+    mask_intervals = (
+        RealSubset([[-np.abs(z), np.abs(z)]])
+        if absolute
+        else RealSubset([[-np.inf, z]])
+    )
+
+    inner_intervals = intervals & mask_intervals
+    outer_intervals = intervals - mask_intervals
+
+    inner_log_area = _compute_log_area(rv, inner_intervals)
+    outer_log_area = _compute_log_area(rv, outer_intervals)
+    # equal to 1.0 / (1.0 + np.exp(outer_log_area - inner_log_area))
+    return np.exp(-np.log1p(np.exp(outer_log_area - inner_log_area)))
+
+
+def _log1mexp(z: np.ndarray) -> np.ndarray:
+    """Compute the logarithm of one minus the exponential of the input array, element-wise.
+
+    Args:
+        z (np.ndarray): Input values.
+
+    Returns:
+        np.ndarray: Logarithm of one minus the exponential of the input array.
+    """
+    z = np.asarray(z)
+    values = np.empty_like(z)
+    halflog = -0.693147  # equal to log(0.5)
+    mask = z < halflog
+    values[mask] = np.log1p(-np.exp(z[mask]))
+    values[~mask] = np.log(-np.expm1(z[~mask]))
+    return values
+
+
+def _compute_log_area(rv: rv_continuous, intervals: RealSubset) -> float:
+    """Compute the logarithm of the integral of the pdf over the each interval.
+
+    Args:
+        rv (rv_continuous): The rv_continuous instance to be integrated.
+        intervals (RealSubset): The intervals on which to compute the integral.
+
+    Returns:
+        float: The logarithm of the integral.
+    """
+    left_ends, right_ends = intervals.intervals.T
+    log_each_area = np.empty(len(intervals))
+    mask = left_ends < rv.median()
+
+    left_log_cdf, right_log_cdf = (
+        rv.logcdf(left_ends[mask]),
+        rv.logcdf(right_ends[mask]),
+    )
+    log_each_area[mask] = right_log_cdf + _log1mexp(left_log_cdf - right_log_cdf)
+
+    left_log_sf, right_log_sf = rv.logsf(left_ends[~mask]), rv.logsf(right_ends[~mask])
+    log_each_area[~mask] = left_log_sf + _log1mexp(right_log_sf - left_log_sf)
+
+    return logsumexp(log_each_area)
--- tests/test_core_cdf.py
+++ tests/test_core_cdf.py
@@ -3,58 +3,50 @@
 import numpy as np
 import pytest
 from numpy.testing import assert_allclose
+from scipy.stats import chi, norm, rv_continuous  # type: ignore[import]
 
-from sicore.core.cdf import truncated_chi_cdf, truncated_norm_cdf
+from sicore.core.cdf import truncated_cdf
 
 
 @pytest.mark.parametrize(
-    ("z", "intervals", "absolute", "expected"),
+    ("rv", "z", "intervals", "absolute", "expected"),
     [
-        (-np.inf, [[-np.inf, -2.0], [2.0, 7.0]], False, 0.0),
-        (-1.7, [[-np.inf, -1.5], [-0.3, 0.5], [1.0, 12.0]], False, 0.08332542),
-        (0.8, [[-1.5, -1.2], [0.1, 2.3], [3.0, 12.0]], False, 0.5942752),
-        (7.3, [[-6.2, -6.1], [7.0, 12.0]], False, 0.99942286),
-        (-4.0, [[-5.0, -4.0], [-2.0, -1.0], [1.0, 3.0], [4.0, 5.0]], True, 0.99978597),
-        (2.0, [[1.0, 3.0], [4.0, 5.0]], True, 0.86378503),
-        (-9.8, [[-np.inf, -19.0], [-10.0, -9.5]], True, 0.95328134),
-        (5.3, [[-10.0, -6.0], [4.0, 4.6], [5.0, 11.0]], True, 0.99802696),
-        (-0.1, [[-7.0, 1.0], [2.0, 3.0]], True, 0.09232818),
-        (-2.6, [[-3.0, -2.0]], True, 0.84526851),
-        (1.9, [[-5.0, -2.0], [1.4, 2.0], [6.0, np.inf]], True, 0.64440857),
-        (3.5, [[3.4, 3.7], [5.0, 5.6]], True, 0.45465432),
+        (norm(), -np.inf, [[-np.inf, -2.0], [2.0, 7.0]], False, 0.0),
+        (norm(), -1.7, [[-np.inf, -1.5], [-0.3, 0.5], [1.0, 12.0]], False, 0.08332542),
+        (norm(), 0.8, [[-1.5, -1.2], [0.1, 2.3], [3.0, 12.0]], False, 0.5942752),
+        (norm(), 7.3, [[-6.2, -6.1], [7.0, 12.0]], False, 0.99942286),
+        (
+            norm(),
+            -4.0,
+            [[-5.0, -4.0], [-2.0, -1.0], [1.0, 3.0], [4.0, 5.0]],
+            True,
+            0.99978597,
+        ),
+        (norm(), 2.0, [[1.0, 3.0], [4.0, 5.0]], True, 0.86378503),
+        (norm(), -9.8, [[-np.inf, -19.0], [-10.0, -9.5]], True, 0.95328134),
+        (norm(), 5.3, [[-10.0, -6.0], [4.0, 4.6], [5.0, 11.0]], True, 0.99802696),
+        (norm(), -0.1, [[-7.0, 1.0], [2.0, 3.0]], True, 0.09232818),
+        (norm(), -2.6, [[-3.0, -2.0]], True, 0.84526851),
+        (norm(), 1.9, [[-5.0, -2.0], [1.4, 2.0], [6.0, np.inf]], True, 0.64440857),
+        (norm(), 3.5, [[3.4, 3.7], [5.0, 5.6]], True, 0.45465432),
+        (chi(14), 2.5, [[1.6, 6.6]], False, 0.03985783),
+        (chi(5), 6.7, [[6.4, 7.3], [18.9, 22.2], [24.7, 27.9]], False, 0.84278428),
+        (chi(7), 2.3, [[0.0, 0.5], [1.0, 1.5], [2.0, np.inf]], False, 0.24739350),
+        (chi(81), 8.2, [[2.1, 12.6], [18.9, 22.2], [24.7, 27.9]], False, 0.13653858),
+        (chi(23), 2.6, [[2.5, 3.3], [5.9, 22.2]], False, 0.0028667),
+        (chi(3), 5.2, [[2.5, 7.2], [15.0, 22.2]], False, 0.99994229),
+        (chi(11), 3.9, [[2.5, 4.2], [8.0, 22.2]], False, 0.89193139),
+        (chi(2), np.inf, [[0.0, 0.5], [1.0, 1.5], [2.0, np.inf]], False, 1.0),
     ],
 )
-def test_truncated_norm_cdf(
+def test_truncated_cdf(
+    rv: rv_continuous,
     z: float,
     intervals: list[list[float]],
     *,
     absolute: bool,
     expected: float,
 ) -> None:
-    """Test the truncated norm cdf function."""
-    value = truncated_norm_cdf(z, intervals, absolute=absolute)
-    assert_allclose(value, expected, rtol=1e-6, atol=1e-6)
-
-
-@pytest.mark.parametrize(
-    ("z", "df", "intervals", "expected"),
-    [
-        (2.5, 14, [[1.6, 6.6]], 0.03985783),
-        (6.7, 5, [[6.4, 7.3], [18.9, 22.2], [24.7, 27.9]], 0.84278428),
-        (2.3, 7, [[0.0, 0.5], [1.0, 1.5], [2.0, np.inf]], 0.24739350),
-        (8.2, 81, [[2.1, 12.6], [18.9, 22.2], [24.7, 27.9]], 0.13653858),
-        (2.6, 23, [[2.5, 3.3], [5.9, 22.2]], 0.0028667),
-        (5.2, 3, [[2.5, 7.2], [15.0, 22.2]], 0.99994229),
-        (3.9, 11, [[2.5, 4.2], [8.0, 22.2]], 0.89193139),
-        (np.inf, 2, [[0.0, 0.5], [1.0, 1.5], [2.0, np.inf]], 1.0),
-    ],
-)
-def test_truncated_chi_cdf(
-    z: float,
-    df: int,
-    intervals: list[list[float]],
-    expected: float,
-) -> None:
-    """Test the truncated chi cdf function."""
-    value = truncated_chi_cdf(z, df, intervals)
+    """Test the truncated cdf function."""
+    value = truncated_cdf(rv, z, intervals, absolute=absolute)
     assert_allclose(value, expected, rtol=1e-6, atol=1e-6)
