51fa66381c43ada5fe72c9c6f6194b306f66de10
shirara1016 <shirara1016@gmail.com>
2024-10-28 01:43:38 +0900

    update api documantation for version 2.3.0

--- docs_make/conf.py
+++ docs_make/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, str(Path("../").resolve()))
 project = "sicore"
 project_copyright = "2024, Takeuchi-Lab"
 author = "Takeuchi-Lab"
-release = "2.2.0"
+release = "2.3.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
